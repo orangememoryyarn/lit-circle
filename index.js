776ce5f3229d1077de6b8097c0487223e74a27df
glowingvoices <glowingvoices@gmail.com>
2024-07-01 19:19:16 -0400

    Confetti!

---
+++ index.js
@@ -0,0 +1,8 @@
+let confetti = new Confetti("blop");
+
+// Edit given parameters
+confetti.setCount(75);
+confetti.setSize(2);
+confetti.setPower(25);
+confetti.setFade(false);
+confetti.destroyTarget(false);
